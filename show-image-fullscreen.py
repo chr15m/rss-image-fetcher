1606080605bf3666a368b3cf79a8b825658aa497
Chris McCormick <chris@mccormick.cx>
2014-09-15 23:33:28 +0800

    Hide mouse, fade image out before fading new one in.

--- show-image-fullscreen.py
+++ show-image-fullscreen.py
@@ -8,6 +8,7 @@ import pygame
 pygame.init()
 screen = pygame.display.set_mode((0, 0))
 pygame.display.toggle_fullscreen()
+pygame.mouse.set_visible(False)
 screensize = (screen.get_width(), screen.get_height())
 
 path = "/tmp/rss-image-fetch"
@@ -18,8 +19,10 @@ reslurp = True
 
 class ImageReloader(FileSystemEventHandler):
 	def __init__(self, *args, **kwargs):
-		self.surface = None
+		self.rate = 4
+		self.surface = pygame.Surface((1, 1))
 		self.size = (0, 0)
+		self.fade = False
 		self.screensize = kwargs.pop("screensize")
 		FileSystemEventHandler.__init__(self, *args, **kwargs)
 	
@@ -27,6 +30,19 @@ class ImageReloader(FileSystemEventHandler):
 		print event.event_type, event.src_path
 		if event.src_path.endswith("/tmp"):
 			self.refresh()
+	
+	def get_surface(self):
+		if self.fade:
+			if self.surface.get_alpha():
+				self.surface.set_alpha(self.surface.get_alpha() - self.rate)
+			else:
+				self.fade = False
+				self.surface = self.new_surface
+				self.size = self.new_size
+				self.surface.set_alpha(0)
+		elif int(self.surface.get_alpha() or 0) < 255:
+			self.surface.set_alpha(self.surface.get_alpha() + self.rate)
+		return self.surface 
 	
 	def refresh(self):
 		try:
@@ -37,8 +53,9 @@ class ImageReloader(FileSystemEventHandler):
 			print "Image refreshed."
 			size = (picture.get_width(), picture.get_height())
 			scale = max(float(size[1]) / self.screensize[1], float(size[0]) / self.screensize[0]);
-			self.size = (int(size[0] / scale), int(size[1] / scale))
-			self.surface = pygame.transform.smoothscale(picture, self.size)
+			self.new_size = (int(size[0] / scale), int(size[1] / scale))
+			self.new_surface = pygame.transform.smoothscale(picture, self.new_size)
+			self.fade = True
 	
 	def shift(self):
 		return ((self.screensize[0] - self.size[0]) / 2, (self.screensize[1] - self.size[1]) / 2)
@@ -57,8 +74,7 @@ if __name__ == "__main__":
 				if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
 					done = True
 			screen.fill((0, 0, 0))
-			if imageloader.surface:
-				screen.blit(imageloader.surface, imageloader.shift())
+			screen.blit(imageloader.get_surface(), imageloader.shift())
 			clock.tick(60)
 			pygame.display.flip()
 	except KeyboardInterrupt:
