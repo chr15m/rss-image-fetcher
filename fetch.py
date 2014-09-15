95e8c7f24980f0954f341ed96b0b0f38e4fc57b1
Chris McCormick <chris@mccormick.cx>
2014-09-15 20:22:33 +0800

    Use RSS feeds in the feed-cache folder instead.

--- fetch.py
+++ fetch.py
@@ -1,14 +1,35 @@
 #!/usr/bin/python
 
+import os
 import sys
+import urllib
+from random import choice
 
 from BeautifulSoup import BeautifulSoup
 import feedparser
 
-if len(sys.argv) > 1:
-	feedurl = feedparser.parse(sys.argv[1])
-	statusupdate = feedurl.entries[0].content
-	soup = BeautifulSoup(statusupdate[0]['value'])
-	print([i["src"] for i in soup.findAll("img")])
-else:
-	print "Usage:", sys.argv[0], "http://feed-url/"
+feeds = [x for x in os.listdir("feed-cache") if x.endswith(".xml")]
+
+if feeds:
+	# fetch a randomly selected feed
+	feed_file = choice(feeds)
+	feed = feedparser.parse(os.path.join("feed-cache", feed_file))
+	feed_title = feed.get("feed", {}).get("title", "")
+	print feed_title
+	images = []
+	# randomly select an entry in the feed
+	entry = choice(feed.entries)
+	print entry["title"]
+	if hasattr(entry, "content"):
+		post = entry.content.pop().value
+	elif hasattr(entry, "summary_detail"):
+		post = entry.summary_detail.value
+	else:
+		post = entry.summary
+	# post = getattr(entry, "content", None) or getattr(entry, "value")
+	soup = BeautifulSoup(post)
+	img_url = choice([i["src"] for i in soup.findAll("img")])
+	extension = img_url.split(".").pop()
+	f = open('tmp.' + extension, 'wb')
+	f.write(urllib.urlopen(img_url).read())
+	f.close()
